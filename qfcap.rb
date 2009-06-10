5faccc0c47853042f9ba7ba9cb19e965929d323a
Kenny Stone <kennethstone@gmail.com>
2009-06-10 14:58:37 -0500

    add initial capistrano file

---
+++ qfcap.rb
@@ -0,0 +1,27 @@
+
+
+role :libs, 'ec2-174-129-139-145.compute-1.amazonaws.com'
+ssh_options[:keys] = '/Users/kstone/.ec2/trial1.pem'
+set :user, 'root'
+set :use_sudo, false
+
+task :ls do 
+  run 'ls -al' do |channel, stream, data|
+    puts stream
+    puts data
+  end
+end
+
+task :checkout do
+  capture 'svn export https://svn.sourceforge.net/svnroot/quickfix/trunk/quickfix /mnt/qf'
+end
+
+task :make do
+  capture 'cd /mnt/qf && ./bootstrap'
+  capture 'cd /mnt/qf && ./configure'
+  capture 'cd /mnt/qf && make'
+end
+
+task :run_at do
+  capture 'cd /mnt/qf/test && ./runat 5001'
+end

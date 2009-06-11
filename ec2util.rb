f62dee70a312d34aa05a9b7a8cd4cc71bf8f8df1
Kenny Stone <kennethstone@gmail.com>
2009-06-11 00:38:32 -0500

    incremental progress

--- ec2util.rb
+++ ec2util.rb
@@ -3,7 +3,6 @@ require 'yaml'
 require 'right_aws'
 
 class EC2Util
-
   def self.get_ec2( config )
     RightAws::Ec2.new(config['access_key'], config['secret_access_key'])
   end
@@ -12,8 +11,8 @@ class EC2Util
     ec2 = get_ec2( config )
     image_name = config['builds'][build]['image']
     image_id = config['images'][image_name]
-    i = ec2.launch_instances( image_id, :key_name=>config['ssh_key_name'] )
-    return i[0][:aws_instance_id]
+    inst = ec2.launch_instances( image_id, :key_name=>config['ssh_key_name'] )
+    return inst[0][:aws_instance_id]
   end
 
   def self.describe( config )
@@ -21,21 +20,30 @@ class EC2Util
     puts instances.size.to_s + ' total instances running'
     instances.each do |instance|
       puts '-------------------------------------'
-      instance.each { |param, val| puts param.to_s + ': ' + val.to_s }
+      EC2Util.inst_to_s( instance )
     end
   end
 
+  def self.inst_to_s( instance )
+    instance.each { |param, val| puts param.to_s + ': ' + val.to_s }
+  end
+
+  def self.describe_inst( inst_id, config )
+    puts EC2Util.inst_to_s( EC2Util.get_instance( inst_id, config ) )
+  end
+
   def self.running?( inst_id, config )
-    running = false
-    instances = get_ec2( config ).describe_instances
-    instances.each do |i| 
-      running=true if( i[:aws_state].eql?( 'running' ) and i[:aws_instance_id].eql?( inst_id ))
-    end
-    return running
+    instance = EC2Util.get_instance(inst_id, config )
+    return instance[:aws_state].eql?( 'running' )
   end
 
   def self.stop( inst_id, config )
     get_ec2( config ).terminate_instances( inst_id )
+  end
+
+  def self.get_instance( inst_id, config )
+    instances = get_ec2( config ).describe_instances
+    return instances.select{|i| i[:aws_instance_id]==inst_id }[0]
   end
 
   def self.stop_all( config )
@@ -43,4 +51,11 @@ class EC2Util
     instances = ec2.describe_instances
     instances.each { |inst| ec2.terminate_instances( inst[:aws_instance_id] ) }
   end
+
+  def self.get_dns( inst_id, config )
+    instance = get_instance( inst_id, config )
+    raise 'could not find instance!' if instance.nil?
+    return instance[:dns_name]
+  end
 end
+
--- qfcap.rb
+++ qfcap.rb
@@ -1,15 +1,50 @@
-role :libs, 'ec2-174-129-139-145.compute-1.amazonaws.com'
+require 'yaml'
+require 'ec2util'
+
 ssh_options[:keys] = '/Users/kstone/.ec2/trial1.pem'
 set :user, 'root'
 set :use_sudo, false
 
+@config = YAML::load( File.open('config/build.yml') )
+
+task :launch do
+  abort 'build arg required' if build.nil?
+  inst_id = EC2Util.start( build, @config )
+  puts '-----------------------------------------'
+  puts 'instance id: ' + inst_id.to_s
+  puts '-----------------------------------------'
+  puts 'waiting for instance to start...'
+  30.times do # == ten minutes
+    if( EC2Util.running?( inst_id, @config ) )
+      break
+    else
+      sleep( 20 )
+    end
+  end
+  abort 'instance failed to start!' unless( EC2Util.running?( inst_id, @config ) )
+  EC2Util.describe_inst( inst_id, @config )
+  puts '-----------------------------------------'
+end
+
+task :describe_all do
+  EC2Util.describe( @config )
+end
+
+task :stop_all do
+  EC2Util.stop_all( @config )
+  sleep(10)
+  EC2Util.describe( @config )
+end
+
 task :checkout do
-  capture 'svn export https://svn.sourceforge.net/svnroot/quickfix/trunk/quickfix /mnt/qf'
+  abort 'inst_id and build args required' if( build.nil? or inst_id.nil? )
+  role :libs, EC2Util.get_dns( inst_id, @config )
+  capture "svn export #{@config['builds'][build]['repo']} /mnt/qf"
 end
 
 task :make do
   capture 'cd /mnt/qf && ./bootstrap'
-  capture 'cd /mnt/qf && ./configure'
+  capture 'cd /mnt/qf && ./@configure'
   capture 'cd /mnt/qf && make'
 end
 
